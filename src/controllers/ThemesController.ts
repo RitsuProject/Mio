417b657837a205eae665ad05e882b53be38d1994
Sazzo <felipe@sazz.fail>
2020-10-21 14:56:27 -0300

    Add openings.moe to Random Theme endpoint as provider.

--- src/controllers/ThemesController.ts
+++ src/controllers/ThemesController.ts
@@ -1,6 +1,17 @@
 import { Request, Response } from "express";
 import p from "phin";
 
+interface OpeningsMoeResponse {
+  title: string;
+  source: string;
+  file: string;
+  mime: Array<string>;
+  song: {
+    title: string;
+    artist: string;
+  };
+}
+
 export default {
   async getRandomTheme(req: Request, res: Response) {
     const { provider } = req.query;
@@ -22,12 +33,29 @@ export default {
           "animethemes.moe"
         );
 
-        return res.json({
+        res.json({
           name: video.entries[0].theme.anime.name,
           link: animeLink,
           type: video.entries[0].theme.type,
           full: video,
         });
+        break;
+      }
+      case "openingsmoe": {
+        const oMResponse: any = await p({
+          method: "GET",
+          url: "https://openings.moe/api/list.php",
+          parse: "json",
+        });
+
+        const songs = oMResponse.body;
+        const song: OpeningsMoeResponse =
+          songs[Math.floor(Math.random() * songs.length)];
+
+        song.file = `https://openings.moe/video/${song.file}.mp4`;
+
+        res.json(song);
+        break;
       }
     }
   },
