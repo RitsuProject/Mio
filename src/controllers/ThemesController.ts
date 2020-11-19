bbf02f489b3c80820d9684dbfb10f2e47e9693ad
Sazzo <felipe@sazz.fail>
2020-11-18 22:00:22 -0300

    amelia watson song when animethemes is on fire

--- src/controllers/ThemesController.ts
+++ src/controllers/ThemesController.ts
@@ -151,7 +151,7 @@ export default {
           parse: "json",
         }).catch(() => {
           return res.json({
-            name: "Ritsu is unavailable.",
+            name: "K-On!: Ura-On!",
             link: "https://files.catbox.moe/e9k222.mp3",
             type: "go to support server",
             full: {},
