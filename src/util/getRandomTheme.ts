4bbd731c84bfb093d68c9379c6c1849204a6e38f
Sazzo <felipe@sazz.fail>
2021-04-23 11:48:30 -0300

    fix(animethemes/random): 5xx errors when sources array is empty.

--- src/util/getRandomTheme.ts
+++ src/util/getRandomTheme.ts
@@ -1,9 +1,16 @@
 import Theme from '../database/entities/Theme'
 
 export default async function getRandomTheme() {
-  const count = await Theme.countDocuments()
+  // Sometimes the sources array is empty, so, this condition will check if at least 1 entry exists.
+  const sourceUrlExists = {
+    'versions.0.sources.0': { $exists: true },
+  }
+
+  const count = await Theme.countDocuments(sourceUrlExists)
   const randomNumber = Math.floor(Math.random() * count)
-  const randomTheme = await Theme.findOne().skip(randomNumber).exec()
+  const randomTheme = await Theme.findOne(sourceUrlExists)
+    .skip(randomNumber)
+    .exec()
 
   return randomTheme
 }
